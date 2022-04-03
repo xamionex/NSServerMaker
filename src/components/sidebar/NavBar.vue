aaf8064b67121c6bdbe6377bd85ba304f2c64e52
amione <percesvrzic@gmail.com>
2022-04-03 20:35:19 +0200

    navbar*

--- src/components/sidebar/NavBar.vue
+++ src/components/sidebar/NavBar.vue
@@ -1,27 +1,18 @@
 <template>
     <header>
-        <div class="text-gray-500 dark:text-gray-400">
+        <div class="space-y-2 text-gray-500 dark:text-gray-400 dark:bg-black bg-white">
             <nav>
                 <ul>
-                    <li>
-                        <router-link class="btn" to="/">Home</router-link>
-                    </li>
-                    <li>
-                        <router-link class="btn" to="/">Mods</router-link>
-                    </li>
-                    <li>
-                        <router-link class="btn" to="about">About</router-link>
-                    </li>
-                    <li>
-                        <button id="theme-toggle" type="button" class="btn">
-                            <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5">
-                                <use xlink:href="../../assets/icons.svg#dark"></use>
-                            </svg>
-                            <svg id="theme-toggle-light-icon" class="hidden w-5 h-5">
-                                <use xlink:href="../../assets/icons.svg#light"></use>
-                            </svg>
-                        </button>
-                    </li>
+                    <router-link class="btn" to="/">Home</router-link>
+                    <router-link class="btn" to="about">About</router-link>
+                    <button id="theme-toggle" type="button" class="btn">
+                        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5">
+                            <use xlink:href="../../assets/icons.svg#dark"></use>
+                        </svg>
+                        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5">
+                            <use xlink:href="../../assets/icons.svg#light"></use>
+                        </svg>
+                    </button>
                 </ul>
             </nav>
         </div>
