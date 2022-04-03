ac0b7bf5cc6b27803a45e74cfe601bafc73a6fdb
amione <percesvrzic@gmail.com>
2022-04-03 12:55:06 +0200

    dark mode and icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,19 @@
 <template>
     <header>
-        <div>
+        <div class="text-gray-500 dark:text-gray-400">
             <nav>
                 <ul>
-                    <li>
-                        <router-link to="/">Home</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/">Mods</router-link>
-                    </li>
-                    <li>
-                        <router-link to="about">About</router-link>
-                    </li>
+                    <router-link class="btn" to="/">Home</router-link>
+                    <router-link class="btn" to="/">Mods</router-link>
+                    <router-link class="btn" to="about">About</router-link>
+                    <button id="theme-toggle" type="button" class="btn">
+                        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5">
+                            <use xlink:href="../assets/icons.svg#dark"></use>
+                        </svg>
+                        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5">
+                            <use xlink:href="../assets/icons.svg#light"></use>
+                        </svg>
+                    </button>
                 </ul>
             </nav>
         </div>
