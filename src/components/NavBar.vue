b3b5c848b7712f7ec4aa739da4254c9aa0f5700b
amione <percesvrzic@gmail.com>
2022-04-03 18:15:06 +0200

    <li> in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,17 +3,25 @@
         <div class="text-gray-500 dark:text-gray-400">
             <nav>
                 <ul>
-                    <router-link class="btn" to="/">Home</router-link>
-                    <router-link class="btn" to="/">Mods</router-link>
-                    <router-link class="btn" to="about">About</router-link>
-                    <button id="theme-toggle" type="button" class="btn">
-                        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5">
-                            <use xlink:href="../assets/icons.svg#dark"></use>
-                        </svg>
-                        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5">
-                            <use xlink:href="../assets/icons.svg#light"></use>
-                        </svg>
-                    </button>
+                    <li>
+                        <router-link class="btn" to="/">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link class="btn" to="/">Mods</router-link>
+                    </li>
+                    <li>
+                        <router-link class="btn" to="about">About</router-link>
+                    </li>
+                    <li>
+                        <button id="theme-toggle" type="button" class="btn">
+                            <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5">
+                                <use xlink:href="../assets/icons.svg#dark"></use>
+                            </svg>
+                            <svg id="theme-toggle-light-icon" class="hidden w-5 h-5">
+                                <use xlink:href="../assets/icons.svg#light"></use>
+                            </svg>
+                        </button>
+                    </li>
                 </ul>
             </nav>
         </div>
