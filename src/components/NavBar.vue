2f37f9b2e62c8f0947c0c733dba115166ac363e4
amione <percesvrzic@gmail.com>
2022-04-03 12:19:34 +0200

    tailwind and navbar thingy

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <router-link to="/">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/">Mods</router-link>
+                    </li>
+                    <li>
+                        <router-link to="about">About</router-link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "NavBar",
+    };
+</script>
